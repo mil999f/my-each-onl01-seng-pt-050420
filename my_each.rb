64b231f975c85241901691c683063ec8351800dd
Milly Flores <miflo0h@gmail.com>
2020-05-16 18:02:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 def my_each(array)
   # code here
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
+  i=0 
+  while i < array.length 
+    yield(array[i])
+    i += 1 
+  end 
+  array 
 end
